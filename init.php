f754a09fc4ced7e844e9ebdf772fd9313fb5e86e
Davut Budak <111956109+davdav1149@users.noreply.github.com>
2023-05-10 17:56:30 +0300

    Update init.php

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ $mysqlsunucu = "localhost";
 $mysqlkullanici = "root";
 $mysqlsifre = "";
 try {
-    $conn = new PDO("mysql:host=$mysqlsunucu;dbname=clicksuslinktr;charset=utf8", $mysqlkullanici, $mysqlsifre);
+    $conn = new PDO("mysql:host=$mysqlsunucu;dbname=linktr;charset=utf8", $mysqlkullanici, $mysqlsifre);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 catch(PDOException $e)
